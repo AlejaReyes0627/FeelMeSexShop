be356ff981192b94fb7221372edeee1c3e9e2740
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-03-27 16:09:13 -0500

    .

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -10,12 +10,20 @@ import { Router } from '@angular/router';
 export class MenuComponent implements OnInit {
   logoFeelMe:string;
   SloganFeelMe:string;
-  banner1:string
+  banner1:string;
+  icono1:string;
+  icono2:string;
+  icono3:string;
+  icono4:string;
 
   constructor(public router: Router) {
     this.logoFeelMe='/assets/img/logo.png';
     this.SloganFeelMe='/assets/img/logo2.png';
     this.banner1='/assets/img/sliderHomepage/banner1.jpg';
+    this.icono1='/assets/img/icons/box.png';
+    this.icono2='/assets/img/icons/silence.png';
+    this.icono3='/assets/img/icons/dinero.png';
+    this.icono4='/assets/img/icons/whatsapp.png';
    }
 
   ngOnInit(): void {
