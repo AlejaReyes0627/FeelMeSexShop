4eb00ad647156eba0421b4a1a06464b72d62859c
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-03-27 00:39:16 -0500

    mientrastanto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
-import { BannerComponent } from './components/banner/banner.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent,
     MenuComponent
   ],
   imports: [
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,11 +4,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css',
+'./normalize.css']
 })
 export class MenuComponent implements OnInit {
+  logoFeelMe:string;
+  SloganFeelMe:string;
+  banner1:string
 
-  constructor(public router: Router) { }
+  constructor(public router: Router) {
+    this.logoFeelMe='/assets/img/logo.png';
+    this.SloganFeelMe='/assets/img/logo2.png';
+    this.banner1='/assets/img/sliderHomepage/banner1.jpg';
+   }
 
   ngOnInit(): void {
   }
