f3f4c7355e5400ed7059d36528ba246e9e8eea0a
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-03-27 21:39:36 -0500

    medio front hecho

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,21 +9,28 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
   logoFeelMe:string;
+  iconcarrito:string;
   SloganFeelMe:string;
   banner1:string;
   icono1:string;
   icono2:string;
   icono3:string;
   icono4:string;
+  prueba1:string;
+  prueba2:string;
+
 
   constructor(public router: Router) {
     this.logoFeelMe='/assets/img/logo.png';
     this.SloganFeelMe='/assets/img/logo2.png';
+    this.iconcarrito='/assets/img/icons/carrito.ico';
     this.banner1='/assets/img/sliderHomepage/banner1.jpg';
     this.icono1='/assets/img/icons/box.png';
     this.icono2='/assets/img/icons/silence.png';
     this.icono3='/assets/img/icons/dinero.png';
     this.icono4='/assets/img/icons/whatsapp.png';
+    this.prueba1='/assets/img/pruebas/prueba5.jpg';
+    this.prueba2='/assets/img/pruebas/prueba1.jpg';
    }
 
   ngOnInit(): void {
