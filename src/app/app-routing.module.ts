fe39e135721b1445c7c3911f73afc11d0a5ae3ac
Juan Esteban Gomez Roa <juanesgor@gmail.com>
2022-04-09 10:32:44 -0500

    9/04/2022 Juanes Parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './components/productos/productos.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ProductosOfertaComponent } from './components/productosOferta/productosOferta.component';
 
-const routes: Routes = [{path: '', component:MenuComponent}, {path:'productos', component:ProductosComponent}, {path:'inicio', component:MenuComponent} ];
+const routes: Routes = [{path: '', component:MenuComponent}, {path:'productos', component:ProductosComponent}, {path:'inicio', component:MenuComponent}, {path:'produtosOferta', component:ProductosOfertaComponent} ];
 
 @NgModule({
   
