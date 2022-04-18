4ac68727cd3d47b383026addedb23f11f80537cf
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-04-17 20:38:31 -0500

    aleja nunca muere

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,22 +12,15 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 import { PreciosAsequiblesComponent } from './components/precios-asequibles/precios-asequibles.component';
 import { AtencionOportunaComponent } from './components/atencion-oportuna/atencion-oportuna.component';
-<<<<<<< HEAD
-import { RegistroPersonaComponent } from './components/registro-persona/registro-persona.component';
-=======
-import { LoginComponent } from './components/login/login.component';
-<<<<<<< HEAD
-import { EntryPageComponent } from './components/entry-page/entry-page.component';
 
-const routes: Routes = [
-  { path: '', component: EntryPageComponent },
-=======
->>>>>>> 64a126aefd605486e68eedc42298d3793ce513ee
+import { RegistroPersonaComponent } from './components/registro-persona/registro-persona.component';
+import { LoginComponent } from './components/login/login.component';
+import { EntryPageComponent } from './components/entry-page/entry-page.component';
 
 const routes: Routes = [
   { path: '', component: MenuComponent },
   { path: 'productos', component: ProductosComponent  },
->>>>>>> 1c716662b76ff1690b07d25deee0667dbc2498c7
+  { path: '', component: EntryPageComponent },
   { path: 'inicio', component: MenuComponent },
   { path: 'entrada', component: EntryPageComponent},
   { path: 'productos', component: ProductosComponent },
@@ -41,15 +34,8 @@ const routes: Routes = [
   { path: 'quienesSomos', component: QuienesSomosComponent},
   { path: 'preciosAsequibles', component: PreciosAsequiblesComponent},
   { path: 'atencionOportuna', component: AtencionOportunaComponent},
-<<<<<<< HEAD
-  { path: 'registro-persona', component: RegistroPersonaComponent}
-=======
+  { path: 'registro-persona', component: RegistroPersonaComponent},
   { path: 'login', component: LoginComponent},
-<<<<<<< HEAD
-=======
-
->>>>>>> 64a126aefd605486e68eedc42298d3793ce513ee
->>>>>>> 1c716662b76ff1690b07d25deee0667dbc2498c7
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,17 +20,11 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 import { PreciosAsequiblesComponent } from './components/precios-asequibles/precios-asequibles.component';
 import { AtencionOportunaComponent } from './components/atencion-oportuna/atencion-oportuna.component';
-<<<<<<< HEAD
-import { RegistroPersonaComponent } from './components/registro-persona/registro-persona.component';
-=======
-import { LoginComponent } from './components/login/login.component';
-<<<<<<< HEAD
-import { EntryPageComponent } from './components/entry-page/entry-page.component';
-import { FormsModule } from '@angular/forms';
-=======
->>>>>>> 64a126aefd605486e68eedc42298d3793ce513ee
->>>>>>> 1c716662b76ff1690b07d25deee0667dbc2498c7
 
+import { RegistroPersonaComponent } from './components/registro-persona/registro-persona.component';
+
+import { LoginComponent } from './components/login/login.component';
+import { EntryPageComponent } from './components/entry-page/entry-page.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -48,15 +42,11 @@ import { FormsModule } from '@angular/forms';
     QuienesSomosComponent,
     PreciosAsequiblesComponent,
     AtencionOportunaComponent,
-<<<<<<< HEAD
+
     RegistroPersonaComponent,
-=======
+
     LoginComponent,
-<<<<<<< HEAD
     EntryPageComponent,
-=======
->>>>>>> 64a126aefd605486e68eedc42298d3793ce513ee
->>>>>>> 1c716662b76ff1690b07d25deee0667dbc2498c7
   ],
   imports: [
     BrowserModule,
