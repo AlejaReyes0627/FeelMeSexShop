baf52f84fb3c3a09f482d5f97d7a4a1c22dd1e55
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-05-08 19:44:08 -0500

    arreglado

--- src/app/components/productosOferta/productosOferta.component.ts
+++ src/app/components/productosOferta/productosOferta.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { SexshopdatabaseService } from 'src/app/sexshopdatabase.service';
+import Swal from 'sweetalert2';
 interface Producto {
   idProducto: number;
   nombreProducto: string;
@@ -27,6 +28,8 @@ export class ProductosOfertaComponent implements OnInit {
   listaProductosVibradores: Producto[] = [];
   listaProductosLenceria: Producto[] = [];
   listaProductosOtros: Producto[] = [];
+  listaGenerica: Producto[] = [];
+  listaproductosSeleccionados: Producto[] = [];
   constructor(public router: Router, http: HttpClient) {
     this.sexshopService = new SexshopdatabaseService(http);
     this.logoFeelMe = '/assets/img/logo.png';
@@ -39,10 +42,11 @@ export class ProductosOfertaComponent implements OnInit {
   ngOnInit(): void {
     this.productosOfertaVibradores()
     this.productosOfertaLenceria()
-  
+    console.log("Entrando data:", this.listaproductosSeleccionados);
+
   }
 
-  productosOfertaVibradores(){
+  productosOfertaVibradores() {
     var tipo = 'select';
     var sql = 'SELECT * FROM producto WHERE idCategoria=4 ORDER BY precio ASC  LIMIT 3;';
 
@@ -52,12 +56,13 @@ export class ProductosOfertaComponent implements OnInit {
       if (data.success == true) {
         for (let i in data.mensaje) {
           this.listaProductosVibradores.push(data.mensaje[i]);
+          this.listaGenerica.push(data.mensaje[i]);
         }
       }
     });
 
   }
-  productosOfertaLenceria(){
+  productosOfertaLenceria() {
     var tipo = 'select';
     var sql = 'SELECT * FROM producto WHERE idCategoria=6 ORDER BY precio ASC  LIMIT 3;';
 
@@ -67,26 +72,71 @@ export class ProductosOfertaComponent implements OnInit {
       if (data.success == true) {
         for (let i in data.mensaje) {
           this.listaProductosLenceria.push(data.mensaje[i]);
+          this.listaGenerica.push(data.mensaje[i]);
         }
       }
     });
 
   }
-  productosOfertaOtros(){
+  /*productosOfertaOtros() {
     var tipo = 'select';
     var sql = 'SELECT * FROM producto WHERE precio <= 120000 AND idCategoria=8;';
 
-    this.listaProductosOtros = [];
+    this.listaProductosGenericos = [];
 
     this.sexshopService.llamadoHttp(tipo, sql).subscribe((data: any) => {
       if (data.success == true) {
         for (let i in data.mensaje) {
-          this.listaProductosOtros.push(data.mensaje[i]);
+          this.listaProductosGenericos.push(data.mensaje[i]);
         }
       }
     });
+  }*/
 
+  public encontrarproducto(id: number) {
+    for (let i = 0; i < this.listaGenerica.length; i++) {
+      const element = this.listaGenerica[i];
+      if (element.idProducto == id) {
+        return element;
+      }
+    }
+    return null;
+  }
+  public seleccionarProducto(id: number) {
+    console.log(id);
+    var producto = this.encontrarproducto(id);
+    if (producto != null) {
+      this.listaproductosSeleccionados.push(producto);
+      localStorage.setItem('localProducts', JSON.stringify((this.listaproductosSeleccionados)));
+      Swal.fire({
+        title: 'Éxito!',
+        text: 'Se ha añadido al carrito con exito!',
+        icon: 'success',
+        confirmButtonText: 'Ok',
+      });
+    }
+    console.log(this.listaproductosSeleccionados);
   }
 
-  
+  estaSeleccionado(id: number) {
+    for (let i = 0; i < this.listaproductosSeleccionados.length; i++) {
+      const element = this.listaproductosSeleccionados[i];
+      if (element.idProducto == id) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+
+
+  public deseleccionarProducto(id: number) {
+    console.log(id);
+    var i = this.estaSeleccionado(id);
+    if (i != null) {
+      this.listaproductosSeleccionados.splice(i, 1);
+    }
+  }
+
+
 }
